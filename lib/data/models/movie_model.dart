d1e4cbd1b8066338f33901842e59eeec51cb645c
GilsonAndre <gilsonandre1999@gmail.com>
2023-11-10 14:27:59 -0300

    ligando o model

---
+++ lib/data/models/movie_model.dart
@@ -0,0 +1,27 @@
+class MovieModel {
+  final int id;
+  final String title;
+  final String overView;
+  final double voteAverage;
+  final String posterPath;
+  final String backdropPath;
+
+  MovieModel({
+    required this.id,
+    required this.title,
+    required this.overView,
+    required this.voteAverage,
+    required this.posterPath,
+    required this.backdropPath,
+  });
+
+  factory MovieModel.fromMap(Map<String, dynamic> map) => MovieModel(
+        id: map['id'],
+        title: map['title'],
+        overView: map['overview'],
+        voteAverage: map['vote_average'],
+        posterPath: 'https://image.tmdb.org/t/p/original/${map['poster_path']}',
+        backdropPath:
+            'https://image.tmdb.org/t/p/original/${map['backdrop_path']}',
+      );
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:top_filmes/ui/resorces/strings.dart';
 
 void main() {
   runApp(const MyApp());
@@ -9,10 +10,9 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
+    return const MaterialApp(
+      title: Strings.appName,
       
-    
     );
   }
 }
