438350d9ed521f5fd80f32ca9494e388033c9685
GilsonAndre <gilsonandre1999@gmail.com>
2023-11-13 20:24:54 -0300

    lista de filmes e titulos juntos

--- lib/ui/pages/movies_page.dart
+++ lib/ui/pages/movies_page.dart
@@ -25,48 +25,64 @@ class MoviesPage extends StatelessWidget {
           body: TabBarView(
             children: [
               FutureBuilder(
-                  future: dioRepository.getMovies(),
-                  builder: (context, snapshot) {
-                    if (snapshot.hasError) {
-                      return Text('Desculpe Ocorreu um erro ${snapshot.error}');
-                    } else if (snapshot.hasData) {
-                      final itens = snapshot.data as List<MovieModel>;
-                      return GridView.count(
-                        crossAxisCount: 2,
-                        scrollDirection: Axis.vertical,
-                        children: List.generate(itens.length, (index) {
-                          return InkWell(
-                            onTap: () {},
-                            child: SizedBox(
-                              child: Column(
-                                children: [
-                                  Container(
-                                    height: 200,
+                future: dioRepository.getMovies(),
+                builder: (context, snapshot) {
+                  if (snapshot.hasError) {
+                    return Text('Desculpe Ocorreu um erro ${snapshot.error}');
+                  } else if (snapshot.hasData) {
+                    final itens = snapshot.data as List<MovieModel>;
+                    return GridView.count(
+                      crossAxisCount: 2,
+                      scrollDirection: Axis.vertical,
+                      children: List.generate(itens.length, (index) {
+                        return InkWell(
+                          onTap: () {},
+                          child: Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Column(
+                              mainAxisSize: MainAxisSize.max,
+                              children: [
+                                Expanded(
+                                  flex: 4,
+                                  child: Container(
                                     padding: const EdgeInsets.all(8.0),
                                     decoration: BoxDecoration(
                                       image: DecorationImage(
+                                        fit: BoxFit.fill,
                                         image: NetworkImage(
                                           itens[index].posterPath,
                                         ),
                                       ),
                                     ),
                                   ),
-                                  const SizedBox(
-                                    height: 10.0,
+                                ),
+                                // const SizedBox(
+                                //   height: 10.0,
+                                // ),
+                                SizedBox(
+                                  width: MediaQuery.of(context).size.width,
+                                  child: Text(
+                                    itens[index].title,
+                                    style: const TextStyle(fontSize: 16),
                                   ),
-                                  SizedBox(
-                                    child: Text(itens[index].title),
-                                  ),
-                                ],
-                              ),
+                                ),
+                              ],
                             ),
-                          );
-                        }),
-                      );
-                    } else {
-                      return const CircularProgressIndicator();
-                    }
-                  }),
+                          ),
+                        );
+                      }),
+                    );
+                  } else {
+                    return const Center(
+                      child: SizedBox(
+                        height: 100,
+                        width: 100,
+                        child: CircularProgressIndicator(),
+                      ),
+                    );
+                  }
+                },
+              ),
               const Text('Filmes Mais avaliados'),
             ],
           ),
